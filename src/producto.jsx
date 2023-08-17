bb16395b020e0aec046297268c81012921352b2d
JeronimoLedesma <104276729+TheBlueJL@users.noreply.github.com>
2023-08-17 10:13:55 -0300

    Add files via upload

---
+++ src/producto.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './Styles/index.css'
+import './Styles/App.css'
+import Header from './Components/Header/Header'
+import './Styles/global.css'
+import Footer from './Components/Footer/Footer'
+import SearchBar from './Components/SearchBar'
+import GamesDisplay from './Components/GamesDisplay'
+import ProductoYMas from './Components/ProductoYMas'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+            <Header/>
+            <ProductoYMas/>
+            <Footer/>
+    </React.StrictMode>,
+    )
